31e7b19095a223d1aedcc0ef87e16373ab56ef9b
sandeeprudola <sandeeprudola>
2025-08-11 13:09:06 +0530

    fixes-4

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ const cors=require("cors");
 const rootRouter = require("./routes/index");
 
 const app=express();
-
 app.use(cors());
 app.use(express.json())
+const PORT = process.env.PORT || 3000;
 app.use('/api/v1',rootRouter);
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
